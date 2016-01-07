e61448d2de91e9a95b0fd74c365d3644ca9f2085
Riccardo Casatta <riccardo.casatta@gmail.com>
2016-01-07 18:23:21 +0100

    blockchain rpc batch iterator

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import config
+import time
 
 conf = config.read_default_config();
 
-# rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%( conf['rpcuser'] , conf['rpcpassword'] ) )
 best_block_hash = rpc_connection.getbestblockhash()
-print(rpc_connection.getblock(best_block_hash))
+print(best_block_hash)
 
-# batch support : print timestamps of blocks 0 to 99 in 2 RPC round-trips:
-last = 392147
-blockof = 100
-iteration = last/blockof
-for iternow in range(iteration) :
-  commands = [ [ "getblockhash", iternow * blockof + height] for height in range(100) ]
+starttime = time.time()
+start = 362372  # first block with EW messages
+last  = 392173
+n = 100
+ewtxs = []
+blocks = range(start, last)
+block_chunks = [blocks[i:i+n] for i in range(0, last-start, n) ]
+for iternow in block_chunks :
+  startchunk = time.time()
+  txs_in_chunk = 0
+  ewtxs_num = 0
+  #print(iternow)
+  commands = [ [ "getblockhash", b] for b in iternow ]
   block_hashes = rpc_connection.batch_(commands)
   blocks = rpc_connection.batch_([ [ "getblock", h ] for h in block_hashes ])
-  block_times = [ block["time"] for block in blocks ]
-  print( str(iternow) + " of " + str(iteration) + " time:" + str(block_times[0]) )
+  for block in blocks :
+    txs = block['tx']
+    txs_num = len(txs)
+    txs_in_chunk = txs_in_chunk + txs_num;
+    # print( "txs in block " + str(block['height']) + ":" + str(txs_num) )
+    chunks = [txs[i:i+n] for i in range(0, txs_num, n) ]
+    for chunk in chunks :
+      txs_data = rpc_connection.batch_( [ ["getrawtransaction", x] for x in chunk ] )
+      for tx_data in txs_data :
+        if "455720" in tx_data:
+          ewtxs.append(tx_data)
+          ewtxs_num = ewtxs_num + 1
+  endchunk = time.time() - startchunk
+  print("start_block=" + str(iternow[0]) + " took " + str(endchunk) + "s there was " + str(txs_in_chunk) + " of which of ew " + str(ewtxs_num) )
 
+print( "tx found: " + str(len(ewtxs))  )
