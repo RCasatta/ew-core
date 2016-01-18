10d88ff2f6660f0ef47fd00efde58a5b33775310
Riccardo Casatta <riccardo.casatta@gmail.com>
2016-01-18 12:59:32 +0100

    while true loop, fixing error of watching tx content

--- ew_unconfirmed_nobatch.py
+++ ew_unconfirmed_nobatch.py
@@ -5,6 +5,7 @@ import sys
 import httplib
 import decimal
 import config
+import time
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 def decimal_default(obj):
@@ -14,47 +15,45 @@ def decimal_default(obj):
 
 conf = config.read_default_config();
 
-processed_file = '/tmp/processed_txs'
-
-try:
-  f = open(processed_file,'r')
-  data = f.read() # python will convert \n to os.linesep
-  f.close() # you can omit in most cases as the destructor will call it
-except IOError:
-  data = []
+processed = []
 
 # print data
 # rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%( conf['rpcuser'] , conf['rpcpassword'] ) )
 
-mempool = rpc_connection.getrawmempool()
-n = 10 # 100 rpc function call every request
-ewtxs = []
-for tx in mempool:
-  if tx not in data:
-    print tx
-    txs_data = rpc_connection.getrawtransaction( tx )
-    for idx, tx_data in enumerate(txs_data):
+
+while 1:
+  starting = time.time()
+  mempool = rpc_connection.getrawmempool()
+  n = 10 # 100 rpc function call every request
+  already_done = 0
+  doing = 0
+  ewtxs = []
+  for tx in mempool:
+    if tx in processed:
+      already_done = already_done + 1
+    else: 
+      tx_data = rpc_connection.getrawtransaction( tx )
+      # print tx + ":" + tx_data
       if "455720" in tx_data:
-        decoded = rpc_tion.decoderawtransaction(tx_data)
+        decoded = rpc_connection.decoderawtransaction(tx_data)
         ewtxs.append(decoded)
 
-print "-------------"
-print "mempool txs " + str(len(mempool))
-print ewtxs;    
+  print "---------------------"
+  print "elapsed: " + str(time.time() - starting)
+  print "mempool txs: " + str(len(mempool))
+  print "already done: " + str(already_done)
+  print "ewtxs: " + str(len(ewtxs)) + " of " + len(mempool) - already_done;
+  processed = mempool   
 
-f = open(processed_file,'w')
-f.write( json.dumps(mempool) ) # python will convert \n to os.linesep
-f.close() # you can omit in most cases as the destructor will call it
-
-if len(ewtxs)>0 : 
-  jsonresult = json.dumps(ewtxs, default=decimal_default)
-  conn = httplib.HTTPConnection('eternitywall.it', 80)
-  conn.connect()
-  conn.request('POST', "/v1/hooks/ewunconfirmedtx" , jsonresult)
-  resp = conn.getresponse()
-  conn.close()
-  print resp.status, resp.reason
+  if len(ewtxs)>0 : 
+    jsonresult = json.dumps(ewtxs, default=decimal_default)
+    conn = httplib.HTTPConnection('eternitywall.it', 80)
+    conn.connect()
+    conn.request('POST', "/v1/hooks/ewunconfirmedtx" , jsonresult)
+    resp = conn.getresponse()
+    conn.close()
+    print resp.status, resp.reason
 
 
  
