4af141b0e532358589553ee55c600b07fd51103b
Riccardo Casatta <riccardo.casatta@gmail.com>
2016-03-03 10:42:03 +0100

    regularexpression

--- ew_unconfirmed.py
+++ ew_unconfirmed.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+import re
+import time
 import json
 import sys
 import httplib
@@ -14,44 +15,51 @@ def decimal_default(obj):
 
 conf = config.read_default_config();
 
-processed = []
-
 # print data
 # rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%( conf['rpcuser'] , conf['rpcpassword'] ) )
-
-
+pattern=re.compile("4557(20|41|43)")
+processed = []
 while 1:
     starting = time.time()
     mempool = rpc_connection.getrawmempool()
-    n = 10 # 100 rpc function call every request
-    mempool_chunks = [mempool[i:i+n] for i in range(0, len(mempool) , n) ]
-    already_done = 0
-    doing = 0
-    ewtxs = []
-    for chunk in mempool_chunks:
-      print chunk
-      txs_data = rpc_connection.batch_( [ ["getrawtransaction", x] for x in chunk ] )
-      for idx, tx_data in enumerate(txs_data):
-        tx = chunk[idx];
-        print "---" + idx + ":" + tx + ":" + tx_data
-
-        if tx in processed:
-            already_done = already_done + 1
-        else:
-            if "4557" in tx_data:
-                decoded = rpc_connection.decoderawtransaction(tx_data)
-                #todo verify OPRETURUN STARTS WITH 455720 455741 455743
-                ewtxs.append(decoded)
-
     print "---------------------"
-    print "elapsed: " + str(time.time() - starting)
-    print "mempool txs: " + str(len(mempool))
-    print "already done: " + str(already_done)
-    print "ewtxs: " + str(len(ewtxs)) + " of " + str(len(mempool) - already_done);
-    processed = mempool
+    print "mempool size: " + str(len(mempool))
+    print "processed size: " + str(len(processed))
+    toprocess = list( set(mempool) - set(processed) )
+    print "toprocess size: " + str(len(toprocess))
+    
+    n = 20
+    containEW = 0;
+    toprocess_chunks = [toprocess[i:i+n] for i in range(0, len(toprocess) , n) ]
 
-    if len(ewtxs)>0 :
+    ewtxs = []
+    processed = []
+
+    for i, chunk in enumerate(toprocess_chunks):
+      print str(i) + "/" + str(len(toprocess_chunks));
+      try:
+        txs_data = rpc_connection.batch_( [ ["getrawtransaction", x] for x in chunk ] )
+        for idx, tx_data in enumerate(txs_data):
+          tx = chunk[idx];
+          processed.append(tx)
+          if pattern.search(tx_data):
+            print "tx_data contains 4547(20|41|43)"
+            containEW = containEW + 1
+            decoded = rpc_connection.decoderawtransaction(tx_data)
+            #todo verify OPRETURUN STARTS WITH 455720 455741 455743
+            ewtxs.append(decoded)
+      except:
+        print "Unexpected error:", sys.exc_info()[0]
+        
+
+    print "elapsed: " + str(time.time() - starting)
+    print "mempool size: " + str(len(mempool))
+    print "processed size: " + str(len(processed))
+    print "containEW: " + str(containEW)
+    print "ewtxs: " + str(len(ewtxs)) + " of " + str(len(mempool) - already_done);
+
+    if len(ewtxs)>0 and false :
         jsonresult = json.dumps(ewtxs, default=decimal_default)
         conn = httplib.HTTPConnection('eternitywall.it', 80)
         conn.connect()
@@ -60,18 +68,3 @@ while 1:
         conn.close()
         print resp.status, resp.reason
     time.sleep(5)
-
-
-
-mempool = rpc_connection.getrawmempool()
-n = 10 # 100 rpc function call every request
-mempool_chunks = [mempool[i:i+n] for i in range(0, len(mempool) , n) ]
-ewtxs = []
-for chunk in mempool_chunks:
-  print chunk
-  txs_data = rpc_connection.batch_( [ ["getrawtransaction", x] for x in chunk ] )
-  for idx, tx_data in enumerate(txs_data):
-    if "455720" in tx_data:
-      ewtxs.append( chunk[idx] )
-
-print ewtxs;
